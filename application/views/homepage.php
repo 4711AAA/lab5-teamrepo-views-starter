60a9be8d157c9e453389cf7b97e71f9415c3eea7
Si-Yang <cameraman03@hotmail.com>
2018-02-14 09:55:39 -0800

    created home page features

---
+++ application/views/homepage.php
@@ -0,0 +1,17 @@
+<div class="alert alert-info"><strong>{remaining_tasks}</strong> tasks are left to do!</div>
+
+<table class="table">
+    <tr>
+        <th>Id</th>
+        <th>Task</th>
+        <th>Priority</th>
+    </tr>
+
+    {display_tasks}
+    <tr>
+        <td>{id}</td>
+        <td>{task}</td>
+        <td>{priority}</td>
+    </tr>
+    {/display_tasks}
+</table>
