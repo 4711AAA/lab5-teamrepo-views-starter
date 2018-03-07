117109d40c1a7c8a7f7a60bf81932809d2249af2
Si-Yang <cameraman03@hotmail.com>
2018-03-07 12:20:18 -0800

    priority tasks in progress

--- tests/application/models/TaskTest.php
+++ tests/application/models/TaskTest.php
@@ -13,6 +13,8 @@ class TaskTest extends TestCase
         $this->tasks = new Tasks();
     }
 
+    /* Task Tests */
+
     // tests task name rules (should pass)
     function testGoodTaskName()
     {
@@ -27,23 +29,53 @@ class TaskTest extends TestCase
     function testEmptyTaskName()
     {
         $emptyTaskName = new Task();
+        $emptyTaskName->task = 'Test test';
         $this->expectException(Exception::class);
-        $emptyTaskName->task = '';
+        $emptyTaskName->setTask('');
+        $this->assertEquals('Test test', (string) $emptyTaskName->task);
     }
 
     // tests the alphanumeric + spaces rule for task names
     function testNotAlphanumericSpacesTaskName()
     {
         $notAlphaNumSpcTaskName = new Task();
+        $notAlphaNumSpcTaskName->task = 'Test test';
         $this->expectException(Exception::class);
-        $notAlphaNumSpcTaskName->task = 'Hello!>?<';
+        $notAlphaNumSpcTaskName->setTask('Hello!>?<');
+        $this->assertEquals('Test test', (string) $notAlphaNumSpcTaskName->task);
     }
 
     // tests the max length rule for task names
     function testTooLongTaskName()
     {
         $tooLongTaskName = new Task();
+        $tooLongTaskName->task = 'Test test';
         $this->expectException(Exception::class);
-        $tooLongTaskName->task = "11111222223333344444555556666611111222223333344444555556666611111";
+        $tooLongTaskName->setTask('11111222223333344444555556666611111222223333344444555556666611111');
+        $this->assertEquals('Test test', (string) $tooLongTaskName->task);
+    }
+
+    /* Priority Tests */
+
+    // test valid priority
+    function testValidPriority () {
+        $goodPriority = new Task();
+
+        $goodPriority->setTask(3);
+        $this->assertEquals(3, $goodPriority->task);
+
+        $goodPriority->setTask(2);
+        $this->assertEquals(2, $goodPriority->task);
+
+        $goodPriority->setTask(1);
+        $this->assertEquals(1, $goodPriority->task);
+    }
+
+    // testing invalid priority entries
+    function testInvalidPriority() {
+        $badPriority = new Task();
+
+        $this->expectException(Exception::class);
+        $badPriority->setPriority(4);
     }
 }
