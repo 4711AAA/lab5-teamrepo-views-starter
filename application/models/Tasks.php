14ba25b57837dab2dbcf124258b0d4053a00be9b
Si-Yang <cameraman03@hotmail.com>
2018-03-20 20:57:52 -0700

    extracting xml in progress

--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,12 +1,13 @@
 <?php
 
-class Tasks extends CSV_Model
+
+class Tasks extends XML_Model
 {
     private $CI;
 
     public function __construct()
     {
-        parent::__construct(APPPATH . '../data/tasks.csv', 'id');
+        parent::__construct(APPPATH . '../data/tasks.xml', 'id');
         $this->CI = &get_instance();
     }
 
@@ -37,12 +38,106 @@ class Tasks extends CSV_Model
     public function rules()
     {
         $config = array(
-            ['field' => 'task', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
+            ['field' => 'desc', 'label' => 'TODO task', 'rules' => 'alpha_numeric_spaces|max_length[64]'],
             ['field' => 'priority', 'label' => 'Priority', 'rules' => 'integer|less_than[4]'],
             ['field' => 'size', 'label' => 'Task size', 'rules' => 'integer|less_than[4]'],
             ['field' => 'group', 'label' => 'Task group', 'rules' => 'integer|less_than[5]'],
         );
         return $config;
+    }
+
+    // Loads data from the xml file
+    protected function load()
+    {
+
+        /*
+        if (($tasks = simplexml_load_file($this->_origin)) !== FALSE)
+        {
+            foreach ($tasks as $task) {
+                $record = new stdClass();
+                $record->id = (int) $task->id;
+                $record->task = (string) $task->task;
+                $record->priority = (int) $task->priority;
+                $record->size = (int) $task->size;
+                $record->group = (int) $task->group;
+                $record->deadline = (string) $task->deadline;
+                $record->status = (int) $task->status;
+                $record->flag = (int) $task->flag;
+
+                $this->_data[$record->id] = $record;
+            }
+        }
+
+        // rebuild the keys table
+        $this->reindex();
+
+        */
+        if (file_exists(realpath($this->_origin))) {
+
+            $this->xml = simplexml_load_file(realpath($this->_origin));
+            if ($this->xml === false) {
+                // error so redirect or handle error
+                header('location: /404.php');
+                exit;
+            }
+
+            $xmlarray =$this->xml;
+
+            //if it is empty;
+            if(empty($xmlarray)) {
+                return;
+            }
+
+            //get all xmlonjects into $xmlcontent
+            $rootkey = key($xmlarray);
+            $xmlcontent = (object)$xmlarray->$rootkey;
+
+            $keyfieldh = array();
+            $first = true;
+
+            //if it is empty;
+            if(empty($xmlcontent)) {
+                return;
+            }
+
+            $dataindex = 1;
+            $first = true;
+            foreach ($xmlcontent as $oj) {
+                if($first){
+                    foreach ($oj as $key => $value) {
+                        $keyfieldh[] = $key;
+//                        var_dump((string)$value);
+                    }
+                    $this->_fields = $keyfieldh;
+                }
+//                $record = new Task();
+//                $record->id = (int) $oj->id;
+//                $record->task = (string) $oj->desc;
+//                $record->priority = (int) $oj->priority;
+//                $record->size = (int) $oj->size;
+//                $record->group = (int) $oj->group;
+//                $record->deadline = (string) $oj->deadline;
+//                $record->status = (int) $oj->status;
+//                $record->flag = (int) $oj->flag;
+//
+//                $this->_data[$record->id] = $record;
+                $first = false;
+
+//                var_dump($oj->children());
+                $one = new stdClass();
+
+                //get objects one by one
+                foreach ($oj as $key => $value) {
+                    $one->$key = (string)$value;
+                }
+                $this->_data[$dataindex++] =$one;
+            }
+
+
+            var_dump($this->_data);
+        } else {
+            exit('Failed to open the xml file.');
+        }
     }
 }
 
